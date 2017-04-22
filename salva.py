8b91df3762a8f82ae8f018668c5ddbc75988b06e
James Peres <james.peres.s@gmail.com>
2017-04-22 11:44:43 -0300

    refatorado pela cris

--- salva.py
+++ salva.py
@@ -1,17 +1,14 @@
 """Feito para salvar as despesas por ano a partir do ano de 2008 em um csv"""
-
-
-
 import urllib.request
 from datetime import datetime
-ano = 2007
-data = datetime.now()
-while ano < 2017:
-    ano = ano + 1
-    nome = str(data.day)+'-'+str(data.month)+'-'+str(data.year) + '-'+str(ano)+".csv"
-    url_pura = 'http://riotransparente.rio.rj.gov.br/arquivos/Open_Data_Desp_'+str(ano)+'.asp'
-    print(url_pura)
-    local_filename,  headers  = urllib.request.urlretrieve(url_pura, nome)
-    print(nome)
 
+start_year = 2008
+current_date = datetime.today()
 
+raw_url = 'http://riotransparente.rio.rj.gov.br/arquivos/Open_Data_Desp_{year}.asp'
+raw_filename = '{extract_date}_{reference_year}.csv'
+
+for year in range(start_year, current_date.year + 1): # include current year in loop
+    url = raw_url.format(year=year)
+    filename = raw_filename.format(extract_date=current_date.strftime('%Y%m%d'), reference_year=year)
+    urllib.request.urlretrieve(url, filename)
