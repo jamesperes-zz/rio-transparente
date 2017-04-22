7db4c5afc7044270acfec2fb649a66c90ef0542c
James Peres <james.peres.s@gmail.com>
2017-04-22 02:03:40 -0300

    salvando em csv banco de informações

---
+++ salva.py
@@ -0,0 +1,13 @@
+import urllib.request
+from datetime import datetime
+ano = 2007
+data = datetime.now()
+while ano < 2017:
+    ano = ano + 1
+    nome = str(data.day)+'-'+str(data.month)+'-'+str(data.year) + '-'+str(ano)+".csv"
+    url_pura = 'http://riotransparente.rio.rj.gov.br/arquivos/Open_Data_Desp_'+str(ano)+'.asp'
+    print(url_pura)
+    local_filename,  headers  = urllib.request.urlretrieve(url_pura, nome)
+    print(nome)
+
+
